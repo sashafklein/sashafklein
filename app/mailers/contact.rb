d2d5d21405c430fcb7b9cc94eb3e2e773b1212d5
Sasha Klein <sashafklein@gmail.com>
2013-02-04 18:15:55 -0800

    Favicon and mail fixes

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -1,12 +1,12 @@
 class Contact < ActionMailer::Base
-  default from: "unimportant@example.com"
+  default from: "contact@sashafklein.com"
 
   def contact_message(email)
   	@name = email.name
   	@address = email.address
   	@subject = email.subject
   	@content = email.content
-    mail(:to => ENV['EMAIL_ADDRESS'], :subject => "SFK: #{@subject}")
+    mail(:to => ENV['EMAIL_ADDRESS'], :subject => "**SFK** - #{@subject}")
   end
 
 end
