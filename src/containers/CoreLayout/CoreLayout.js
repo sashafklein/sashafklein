8851b2b667a28038e5d56c0204dc5e1330c9bb13
Sasha Klein <sashafklein@gmail.com>
2016-12-29 17:28:00 -0800

    Aight. That's it with the refactors

--- src/containers/CoreLayout/CoreLayout.js
+++ src/containers/CoreLayout/CoreLayout.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import Header from '../../components/Header'
+
+import Header from 'components/Header'
+import 'styles/core.scss'
+
 import './CoreLayout.scss'
-import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='container text-center'>
